ff0b715e5016a321a6684dde3167364867d73808
Tatsuya Watanabe <watanabe@ignorabimus.info>
2018-12-19 00:15:51 +0900

    Enhance example/repl.c with replxx library

--- example/msvcbuild.bat
+++ example/msvcbuild.bat
@@ -1,18 +1,34 @@
+@setlocal
+cd ..\..\replxx
+@if not exist build\ (
+  mkdir build
+)
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG" ..
+msbuild replxx.vcxproj /p:Configuration=Release
+@endlocal
+
+@setlocal
 cd ..\src
 call msvcbuild.bat static
+@endlocal
 
+@setlocal
 cd ..\ext\re
 call msvcbuild.bat
+@endlocal
 
-cd ..\..\ext\tsx
-call msvcbuild.bat
-
-cd ..\..\example
 @setlocal
+cd ..\ext\tsx
+call msvcbuild.bat
+@endlocal
+
+@setlocal
+@set REPLXX_H_DIR=..\..\replxx\include
 @set SCHEME_H_DIR=..\src
-@set MSCOMPILE=cl /nologo /O2 /W3 /c /D_CRT_SECURE_NO_WARNINGS /I%SCHEME_H_DIR%
+@set MSCOMPILE=cl /nologo /O2 /W3 /c /D_CRT_SECURE_NO_WARNINGS /I%SCHEME_H_DIR% /I%REPLXX_H_DIR%
 @set MSLINK=link /nologo
-@set EXT_LIBS=..\ext\re\lib\re.lib ..\ext\tsx\lib\tsx.lib
+@set EXT_LIBS=..\ext\re\lib\re.lib ..\ext\tsx\lib\tsx.lib ..\..\replxx\build\Release\replxx.lib
 
 @if not exist bin\ (
   mkdir bin
@@ -22,3 +38,4 @@ cd ..\..\example
 %MSLINK% /out:bin\repl.exe repl.obj ..\src\lib\miniscm.lib %EXT_LIBS%
 
 del repl.obj
+@endlocal
--- example/repl.c
+++ example/repl.c
@@ -1,29 +1,17 @@
 #include "miniscm.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 #ifdef _WIN32
 #include <winsock2.h>
 #endif
 
+#include "replxx.h"
 void init_re(void);
 void init_tsx(void);
-
-static char *getline(const char *prompt)
-{
-	char buf[1024], *p;
-
-	fputs(prompt, stdout);
-
-	if (fgets(buf, sizeof(buf), stdin) == NULL) {
-		return NULL;
-	}
-
-	p = (char *)malloc(strlen(buf) + 1);
-	if (p == NULL) {
-		return NULL;
-	}
-	strcpy(p, buf);
-
-	return p;
-}
+extern pointer oblist;
 
 static int get_input_length(const char *input)
 {
@@ -95,9 +83,52 @@ static int get_input_length(const char *input)
 	return (n_paren > 0 || s_paren > 0 || c_paren > 0) ? -1 : p - input;
 }
 
+static void completionHook(char const* prefix, int bp, replxx_completions* lc, void* ud)
+{
+	char **examples = (char **)ud;
+	size_t i;
+	for (i = 0; examples[i] != NULL; ++i) {
+		if (strncmp(prefix + bp, examples[i], strlen(prefix) - bp) == 0) {
+			replxx_add_completion(lc, examples[i]);
+		}
+	}
+}
+
+static void hintHook(char const* prefix, int bp, replxx_hints* lc, ReplxxColor* c, void* ud)
+{
+	char **examples = (char **)ud;
+	size_t i;
+	int len = strlen(prefix);
+	if (len > bp) {
+		for (i = 0; examples[i] != NULL; ++i) {
+			if (strncmp(prefix + bp, examples[i], strlen(prefix) - bp) == 0) {
+				replxx_add_hint(lc, examples[i] + len - bp);
+			}
+		}
+	}
+}
+
+static void colorHook(char const* str_, ReplxxColor* colors_, int size_, void* ud)
+{
+	int i = 0;
+	for (; i < size_; ++i) {
+		if (isdigit(str_[i])) {
+			colors_[i] = BRIGHTMAGENTA;
+		}
+	}
+}
+
+static int compare_hints(const void *a, const void *b)
+{
+	return strcmp(*(char **)a, *(char **)b);
+}
+
 int main(int argc, char *argv[])
 {
 	int ret;
+	pointer x;
+	char *examples_base = NULL, **examples = NULL;
+	size_t hints_size = 0, hints_num = 0;
 
 #ifdef _WIN32
 	WSADATA wsaData;
@@ -116,6 +147,45 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	for (x = oblist; x != NIL; x = cdr(x)) {
+		hints_size += strlen(symname(car(x))) + 1;
+		hints_num++;
+	}
+
+	examples_base = (char *)malloc(hints_size + 1);
+	if (examples_base == NULL) {
+		fprintf(stderr, "Fatal: malloc failed (%u bytes).\n", hints_size + 1);
+		return 1;
+	}
+
+	examples = (char **)malloc((hints_num + 1) * sizeof (char *));
+	if (examples == NULL) {
+		fprintf(stderr, "Fatal: malloc failed (%u bytes).\n", (hints_num + 1) * sizeof (char *));
+		return 1;
+	}
+
+	hints_size = hints_num = 0;
+	examples[0] = examples_base;
+	for (x = oblist; x != NIL; x = cdr(x)) {
+		hints_size = strlen(symname(car(x))) + 1;
+		memcpy(examples[hints_num], symname(car(x)), hints_size);
+		examples[hints_num + 1] = examples[hints_num] + hints_size;
+		hints_num++;
+	}
+	examples[hints_num] = NULL;
+
+	qsort(examples, hints_num, sizeof (char *), compare_hints);
+
+	char const* prompt0 = "\x1b[1;32m>>>\x1b[0m ";
+	char const* prompt1 = "\x1b[1;32m...\x1b[0m ";
+
+	Replxx* replxx = replxx_init();
+	replxx_install_window_change_handler(replxx);
+	replxx_set_completion_callback(replxx, completionHook, (void *)examples);
+	replxx_set_highlighter_callback(replxx, colorHook, NULL);
+	replxx_set_hint_callback(replxx, hintHook, (void *)examples);
+	replxx_set_word_break_characters(replxx, " \\\"'`;@{([])}");
+
 	if (argc > 1) {
 		FILE *fin = fopen(argv[1], "r");
 		if (fin == NULL) {
@@ -129,33 +199,50 @@ int main(int argc, char *argv[])
 
 	ret = 0;
 	while (ret <= 0) {
-		char *line1 = getline("> ");
-		if (line1 == NULL) {
+		char const* result;
+		do {
+			result = replxx_input(replxx, prompt0);
+		} while ((result == NULL) && (errno == EAGAIN));
+
+		if (result == NULL) {
 			break;
 		}
+		replxx_history_add(replxx, result);
+
+		char *line1 = (char *)malloc(strlen(result) + 2);
+		if (line1 == NULL) {
+			fprintf(stderr, "Fatal: malloc failed (%u bytes).\n", strlen(result) + 2);
+			goto EXIT;
+		}
+		sprintf(line1, "%s\n", result);
 
 		do {
 			int i, len;
 			char *line2, *line3;
 			while ((len = get_input_length(line1)) == -1) {
-				line2 = getline("  ");
-				if (line2 == NULL) {
+				do {
+					result = replxx_input(replxx, prompt1);
+				} while ((result == NULL) && (errno == EAGAIN));
+
+				if (result == NULL) {
 					goto EXIT;
 				}
+				replxx_history_add(replxx, result);
 
-				line3 = (char *)malloc(strlen(line1) + strlen(line2) + 1);
+				line3 = (char *)malloc(strlen(line1) + strlen(result) + 2);
 				if (line3 == NULL) {
+					fprintf(stderr, "Fatal: malloc failed (%u bytes).\n", strlen(line1) + strlen(result) + 2);
 					goto EXIT;
 				}
 
-				sprintf(line3, "%s%s", line1, line2);
+				sprintf(line3, "%s%s\n", line1, result);
 				free(line1);
-				free(line2);
 				line1 = line3;
 			}
 
 			line2 = (char *)malloc(strlen(line1) - len + 1);
 			if (line2 == NULL) {
+				fprintf(stderr, "Fatal: malloc failed (%u bytes).\n", strlen(line1) - len + 1);
 				goto EXIT;
 			}
 			strcpy(line2, line1 + len);
@@ -172,6 +259,7 @@ int main(int argc, char *argv[])
 				line1[len] = '\0';
 				line3 = (char *)malloc(len + 20);
 				if (line3 == NULL) {
+					fprintf(stderr, "Fatal: malloc failed (%u bytes).\n", len + 20);
 					goto EXIT;
 				}
 				sprintf(line3, "(display %s)(newline)", line1);
@@ -188,8 +276,10 @@ int main(int argc, char *argv[])
 	}
 
 EXIT:
+	replxx_end(replxx);
+	free(examples);
+	free(examples_base);
 	scheme_deinit();
-
 #ifdef _WIN32
 	WSACleanup();
 #endif
